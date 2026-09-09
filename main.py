2bbf351a4ec6ba9cb22b119c0ad9d21037df4b51
IceBlue <98395260+IceBlue112vm@users.noreply.github.com>
2026-09-10 00:32:59 +0900

    feat: read current media information

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("OBS Now Playing Lite")
+import asyncio
+
+from winrt.windows.media.control import (
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager,
+)
+
+
+async def main():
+    manager = await MediaManager.request_async()
+    session = manager.get_current_session()
+
+    if session is None:
+        print("No active media session.")
+        return
+
+    media_properties = await session.try_get_media_properties_async()
+
+    print(f"Title: {media_properties.title}")
+    print(f"Artist: {media_properties.artist}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
